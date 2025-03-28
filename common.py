5ceabf7aa3a460e67badf3c86df2b00f0584a591
Cyrus Desai <pseudonym112304@gmail.com>
2025-03-28 15:30:59 -0400

    added preprocessing for ordinality in common file

---
+++ common.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+# Define the custom order
+rank_order = ['UNRANKED', 
+                'IRON I', 'IRON II', 'IRON III', 'IRON IV',
+                'BRONZE I', 'BRONZE II', 'BRONZE III', 'BRONZE IV',
+                'SILVER I', 'SILVER II', 'SILVER III', 'SILVER IV',
+                'GOLD I', 'GOLD II', 'GOLD III', 'GOLD IV',
+                'PLATINUM I', 'PLATINUM II', 'PLATINUM III', 'PLATINUM IV',
+                'EMERALD I', 'EMERALD II', 'EMERALD III', 'EMERALD IV',
+                'DIAMOND I', 'DIAMOND II', 'DIAMOND III', 'DIAMOND IV',
+                'MASTER I', 'GRANDMASTER I', 'CHALLENGER I']
+
+# Create a CategoricalDtype with the specified order
+rank_type = pd.api.types.CategoricalDtype(categories=rank_order, ordered=True)
+
+def get_all_data():
+    players = pd.read_csv('./data/Players.csv')
+    ranked_matches = pd.read_csv('./data/RankedMatches.csv')
+    ranked_player_match_data = pd.read_csv('./data/RankedPlayerMatchData.csv')
+    swiftplay_matches = pd.read_csv('./data/SwiftplayMatches.csv')
+    swiftplay_player_match_data = pd.read_csv('./data/SwiftplayPlayerMatchData.csv')
+
+    return preprocess(players, ranked_matches, ranked_player_match_data, swiftplay_matches, swiftplay_player_match_data)
+
+
+def preprocess(players, ranked_matches, ranked_player_match_data, swiftplay_matches, swiftplay_player_match_data):
+    swiftplay_player_match_data["full_rank"] = swiftplay_player_match_data["tier"] + " " + swiftplay_player_match_data["rank"]
+    swiftplay_player_match_data["full_rank"] = swiftplay_player_match_data["full_rank"].replace("UNRANKED UNRANKED", "UNRANKED")
+
+    ranked_player_match_data["full_rank"] = ranked_player_match_data["tier"] + " " + ranked_player_match_data["rank"]
+    ranked_player_match_data["full_rank"] = ranked_player_match_data["full_rank"].replace("UNRANKED UNRANKED", "UNRANKED")
+
+    players["full_rank"] = players["tier"] + " " + players["rank"]
+    players["full_rank"] = players["full_rank"].replace("UNRANKED UNRANKED", "UNRANKED")
+
+    # Apply the CategoricalDtype to the column
+    swiftplay_player_match_data["full_rank"] = swiftplay_player_match_data["full_rank"].astype(rank_type)
+    ranked_player_match_data["full_rank"] = ranked_player_match_data["full_rank"].astype(rank_type)
+    players["full_rank"] = players["full_rank"].astype(rank_type)
+
+    return players, ranked_matches, ranked_player_match_data, swiftplay_matches, swiftplay_player_match_data
